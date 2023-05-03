acb31cbe0a10b394fb746fb8f4e6ae8d318cb276
Yi Hu <yi_hu@alumni.brown.edu>
2023-05-02 17:34:16 -0700

    [Authentication-Frontend] Implement register function in the frontend. Add flash message

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-const updateCountryFetch = async (username, password) => {
+const loginFetch = async (username, password) => {
     const query = JSON.stringify({
       query: `mutation {
             login(
@@ -14,18 +14,72 @@ const updateCountryFetch = async (username, password) => {
     });
     
     const responseJson = await response.json();
-    return responseJson.data;
+    return responseJson;
   };
+
+const registerFetch = async (username, password) => {
+    const query = JSON.stringify({
+        query: `mutation {
+            user_create(
+                username: "${username}", password: "${password}") {
+                    username
+                }
+            }
+        `,
+        variables: null
+    });
+    const response = await fetch('/graphql', {
+        headers: {'content-type': 'application/json'},
+        method: 'POST',
+        body: query,
+    });
+
+    const responseJson = await response.json();
+    return responseJson;
+};
 
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('user-form');
+    document.getElementById('flash').style.display = 'none';
     form.addEventListener('submit', (event) => {
+        event.preventDefault();
         if (document.activeElement.value == "login") {
-            console.log("here");
-            updateCountryFetch(document.getElementById("username").value, document.getElementById("password").value)
+            if (localStorage.getItem("token")) {
+                document.getElementById('flash').innerHTML = "Already logged in.";
+                document.getElementById('flash').style.display = 'block';
+                document.getElementById('flash').style.color = "red";
+            }
+            else {
+                loginFetch(document.getElementById("username").value, document.getElementById("password").value)
+                .then(res => {
+                    if (!res.data){
+                        document.getElementById('flash').innerHTML = "Login failed.";
+                        document.getElementById('flash').style.display = 'block';
+                        document.getElementById('flash').style.color = "red";
+                    }
+                    else {
+                        document.getElementById('flash').innerHTML = "Login succeeded.";
+                        document.getElementById('flash').style.display = 'block';
+                        document.getElementById('flash').style.color = "black";
+                        localStorage.setItem("token", res.data.login);
+                    }
+                });
+            }
+        }
+        else if (document.activeElement.value == "register") {
+            registerFetch(document.getElementById("username").value, document.getElementById("password").value)
             .then(res => {
-                localStorage.setItem("token", res.login);
-            });
+                if (!res.data) {
+                    document.getElementById('flash').innerHTML = "Registration failed.";
+                    document.getElementById('flash').style.display = 'block';
+                    document.getElementById('flash').style.color = "red";
+                }
+                else {
+                    document.getElementById('flash').innerHTML = "Registration succeeded";
+                    document.getElementById('flash').style.display = 'block';
+                    document.getElementById('flash').style.color = "black";
+                }
+            })
         }
     });
 });
